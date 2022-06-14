94a353a1e3bf155d967a81691ccfc4fe4cdf23bc
FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>
2022-06-14 00:20:58 -0600

    ✅ New tests for Log and related script/actions

---
+++ test/log.test.ts
@@ -0,0 +1,147 @@
+import Log, {LogLevel} from '../src/log';
+import {describe} from 'mocha';
+import {stdout, stderr} from 'test-console';
+import {expect} from 'chai';
+
+const prefix = '[test]';
+const noneLog = new Log(prefix, LogLevel.None);
+const errorLog = new Log(prefix, LogLevel.Error);
+const warningLog = new Log(prefix, LogLevel.Warning);
+const logLog = new Log(prefix, LogLevel.Log);
+const debugLog = new Log(prefix, LogLevel.Debug);
+const timingLog = new Log(prefix, LogLevel.Timing);
+const fnRet = 'function';
+const fn = () => fnRet;
+
+function makeTests(logFn: Log[keyof Log], stdInspect: typeof stdout | typeof stderr, level: LogLevel): void {
+  const fnName = logFn.name.substring(6); // "bound error" eg., remove "bound ";
+  const text = logFn.name + ' asdf';
+  const doExpect = (output: ReadonlyArray<string>, text: string) => {
+    if (level !== LogLevel.Timing) {
+      expect(output).to.deep.equal([prefix + ' ' + text + '\n']);
+    } else {
+      expect(output.length).to.equal(1);
+      expect(output[0]).to.match(new RegExp(prefix.replace(/[\[\]]/g, '\\$&') + ' \\(\\d+\\) ' + text));
+    }
+  };
+  describe(fnName, () => {
+    it(
+      'should output to ' +
+        (stdInspect === stdout ? 'stdout/console.log' : 'stderr/console.error') +
+        ' on LogLevel.' +
+        LogLevel[level],
+      () => {
+        const output = stdInspect.inspectSync(() => {
+          logFn(text);
+        });
+
+        doExpect(output, text);
+      },
+    );
+    it('should not output to ' + (stdInspect !== stdout ? 'stdout/console.log' : 'stderr/console.error'), () => {
+      const otherInspect = stdInspect === stdout ? stderr : stdout;
+      let output;
+      stdInspect.inspectSync(() => {
+        output = otherInspect.inspectSync(() => {
+          logFn(text);
+        });
+      });
+
+      expect(output.length).to.equal(0);
+    });
+    it('should not output on LogLevel.None', () => {
+      const output = stdInspect.inspectSync(() => {
+        noneLog[fnName](text);
+      });
+
+      expect(output.length).to.equal(0);
+    });
+    if (LogLevel.Error !== level) {
+      it('should ' + (LogLevel.Error < level ? 'not ' : '') + 'output on LogLevel.Error', () => {
+        const output = stdInspect.inspectSync(() => {
+          errorLog[fnName](text);
+        });
+
+        if (LogLevel.Error < level) {
+          expect(output.length).to.equal(0);
+        } else {
+          doExpect(output, text);
+        }
+      });
+    }
+    if (LogLevel.Warning !== level) {
+      it('should ' + (LogLevel.Warning < level ? 'not ' : '') + 'output on LogLevel.Warning', () => {
+        const output = stdInspect.inspectSync(() => {
+          warningLog[fnName](text);
+        });
+
+        if (LogLevel.Warning < level) {
+          expect(output.length).to.equal(0);
+        } else {
+          doExpect(output, text);
+        }
+      });
+    }
+    if (LogLevel.Log !== level) {
+      it('should ' + (LogLevel.Log < level ? 'not ' : '') + 'output on LogLevel.Log', () => {
+        const output = stdInspect.inspectSync(() => {
+          logLog[fnName](text);
+        });
+
+        if (LogLevel.Log < level) {
+          expect(output.length).to.equal(0);
+        } else {
+          doExpect(output, text);
+        }
+      });
+    }
+    if (LogLevel.Debug !== level) {
+      it('should ' + (LogLevel.Debug < level ? 'not ' : '') + 'output on LogLevel.Debug', () => {
+        const output = stdInspect.inspectSync(() => {
+          debugLog[fnName](text);
+        });
+
+        if (LogLevel.Debug < level) {
+          expect(output.length).to.equal(0);
+        } else {
+          doExpect(output, text);
+        }
+      });
+    }
+    if (LogLevel.Timing !== level) {
+      it('should ' + (LogLevel.Timing < level ? 'not ' : '') + 'output on LogLevel.Timing', () => {
+        const output = stdInspect.inspectSync(() => {
+          timingLog[fnName](text);
+        });
+
+        if (LogLevel.Timing < level) {
+          expect(output.length).to.equal(0);
+        } else {
+          doExpect(output, text);
+        }
+      });
+    }
+    it('should resolve function arguments before output', () => {
+      const output = stdInspect.inspectSync(() => {
+        logFn(fn);
+      });
+
+      doExpect(output, fnRet);
+    });
+    it('should resolve mixed arguments before output', () => {
+      const output = stdInspect.inspectSync(() => {
+        logFn(text, fn);
+      });
+
+      doExpect(output, text + ' ' + fnRet);
+    });
+  });
+}
+
+describe('Log', () => {
+  makeTests(errorLog.error.bind(errorLog), stderr, LogLevel.Error);
+  makeTests(warningLog.warn.bind(warningLog), stderr, LogLevel.Warning);
+  makeTests(logLog.log.bind(logLog), stdout, LogLevel.Log);
+  makeTests(debugLog.debug.bind(debugLog), stdout, LogLevel.Debug);
+  makeTests(timingLog.timing.bind(timingLog), stdout, LogLevel.Timing);
+});
