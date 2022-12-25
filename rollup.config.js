91afdb6fb3ab66fa9305f548f12c46741482266f
FinalDoom <677609+FinalDoom@users.noreply.github.com>
2022-12-24 22:03:53 -0700

    🐛 Fix downloadURL reference
    goes with automatic release generation and using package name all over

--- rollup.config.js
+++ rollup.config.js
@@ -44,16 +44,8 @@ export default {
         homepage: pkg.homepage,
         author: pkg.author,
         license: pkg.license,
+        downloadURL: `${pkg.homepage}/releases/latest/download/${pkg.name}.user.js`,
       },
-    }),
-    replace({
-      delimiters: ['', '\\b(?!\\.)'],
-      values: {
-        // Sadly downloadUrl is not supported by the metablock plugin, but this hack works.
-        '// @author': `// @downloadUrl ${pkg.homepage}/releases/latest/download/eligibility-checker.user.js
-// @author`,
-      },
-      preventAssignment: true,
     }),
     scss({
       insert: true,
