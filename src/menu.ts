5a27074829126cd200ceff5bf42e0b6d4657b1f2
FinalDoom <677609+FinalDoom@users.noreply.github.com>
2022-12-29 01:17:28 -0700

    💄 Fix info-level log menu item

--- src/menu.ts
+++ src/menu.ts
@@ -7,7 +7,7 @@ export const buildMenu = () => {
   GM.registerMenuCommand('Set Log Level to Trace', () => log.setLevel('TRACE'), 'T');
   GM.registerMenuCommand('Set Log Level to Debug', () => log.setLevel('DEBUG'), 'D');
   GM.registerMenuCommand('Set Log Level to Warning', () => log.setLevel('WARN'), 'W');
-  GM.registerMenuCommand('Set Log Level to Log', () => log.setLevel('INFO'), 'L');
+  GM.registerMenuCommand('Set Log Level to Info', () => log.setLevel('INFO'), 'I');
   GM.registerMenuCommand('Set Log Level to Error', () => log.setLevel('ERROR'), 'E');
   GM.registerMenuCommand('Turn off logging', () => log.setLevel('SILENT'), 'o');
 };
